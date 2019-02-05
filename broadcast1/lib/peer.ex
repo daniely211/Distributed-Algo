d9870cf0cacdbc0a067ba7ef670438d43fa7ea48
Daniel <danielyung211@gmail.com>
2019-02-05 15:31:23 +0000

    Changed broadcast 1 to do a broadcast instead of sending to one 1000 times

--- broadcast1/lib/peer.ex
+++ broadcast1/lib/peer.ex
@@ -24,28 +24,30 @@ defmodule Peer do
   defp listen_broadcast(peers, index, sent, received) do
     receive do
       { :broadcast, max_broadcasts, timeout} ->
+        Process.send_after(self(), {:timeout}, timeout)
         broadcast(peers, max_broadcasts, 1, index, 0, timeout, sent, received)
     end
   end
 
   defp broadcast(peers, max_broadcast, num_broadcast, self_index, recipient_index, timeout, sent, received) do
-
-    # broadcast needs to be in a send and receive loop
-    if num_broadcast > max_broadcast do
-      # we have already sent enough messages to this recipient index, move to the next one
-      if recipient_index + 1 > length(peers) - 1 do
-        # we have sent enough messages to everyone stop.
-        print_message("Peer #{self_index}:", sent, received, 0)
+    receive do
+      {:timeout} -> print_message("Peer #{self_index}:", sent, received, 0)
+    after 0 ->
+      pid = self()
+      # IO.puts "Sending a message from a peer! #{inspect pid}"
+      if recipient_index > length(peers) - 1 do
+        # you have sent to all your peers increment the boradcast by 1
+        if num_broadcast + 1 > max_broadcast do
+          # sent enough broadcast to everyone! TIME TO STOP
+          print_message("Peer #{self_index}:", sent, received, 0)
+        else
+          broadcast(peers, max_broadcast, num_broadcast + 1, self_index, 0, timeout, sent, received)
+        end
       else
-        broadcast(peers, max_broadcast, 1, self_index, recipient_index + 1, timeout, sent, received)
+        send Enum.at(peers, recipient_index), {:received, self_index} # send the message to the recipient
+        new_sent = List.update_at(sent, recipient_index, fn x -> x + 1 end)
+        listen(peers, max_broadcast, num_broadcast, self_index, recipient_index + 1, timeout, new_sent, received)
       end
-    else
-      # pid = self()
-      # IO.puts "Sending a message to a peer! #{inspect pid}"
-      send Enum.at(peers, recipient_index), {:received, self_index} # send the message to the recipient
-      # Update the sent list
-      new_sent = List.update_at(sent, recipient_index, fn x -> x + 1 end)
-      listen(peers, max_broadcast, num_broadcast + 1, self_index, recipient_index, timeout, new_sent, received)
     end
   end
 
@@ -53,10 +55,11 @@ defmodule Peer do
     # pid = self()
     receive do
     { :received, sender_index } ->
-        # IO.puts "received a message from a peer! #{inspect pid}"
+      # IO.puts "received a message from a peer! #{inspect pid}"
       # Received a message from sender
       new_received = List.update_at(received, sender_index, fn x -> x + 1 end)
       broadcast(peers, max_broadcast, num_broadcast, self_index, recipient_index, timeout, sent, new_received)
+    {:timeout} -> print_message("Peer #{self_index}:", sent, received, 0)
     after 
       timeout ->
       print_message("Peer #{self_index}:", sent, received, 0)
