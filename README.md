936a8c8f54fb767305fa81b753373e87b05e37b1
Daniel <danielyung211@gmail.com>
2019-02-09 11:35:29 +0000

    Working on docker for q1 where it sends and receive more than 1000

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Distributed-Algo
+# Distributed-Algorithm course work
+
+Our implementation is split into 6 different folders. In each folder the command "make runall" will run all 3 different version of the implementation locally.
+
+
--- broadcast1/lib/broadcast1.ex
+++ broadcast1/lib/broadcast1.ex
@@ -27,22 +27,36 @@ defmodule Broadcast1 do
   end
 
   def main_net do
-    p0 = Node.spawn(:'peer0@peer0.localdomain', Peer, :start, [0])
-    p1 = Node.spawn(:'peer1@peer1.localdomain', Peer, :start, [1])
-    p2 = Node.spawn(:'peer2@peer2.localdomain', Peer, :start, [2])
-    p3 = Node.spawn(:'peer3@peer3.localdomain', Peer, :start, [3])
-    p4 = Node.spawn(:'peer4@peer4.localdomain', Peer, :start, [4])
+    args = for arg <- System.argv, do: String.to_integer(arg)
+    version = Enum.at(args, 0)
+    num_peers = Enum.at(args, 1)
+
+    p0 = Node.spawn(:'peer0@peer0.localdomain', Peer, :start, [0, num_peers])
+    p1 = Node.spawn(:'peer1@peer1.localdomain', Peer, :start, [1, num_peers])
+    p2 = Node.spawn(:'peer2@peer2.localdomain', Peer, :start, [2, num_peers])
+    p3 = Node.spawn(:'peer3@peer3.localdomain', Peer, :start, [3, num_peers])
+    p4 = Node.spawn(:'peer4@peer4.localdomain', Peer, :start, [4, num_peers])
     peers = [p0, p1, p2, p3, p4]
 
     Enum.map(peers, fn(peer) ->
       send peer, { :peers, peers }
     end)
 
-    Enum.map(peers, fn(peer) ->
-      send peer, { :broadcast, 1000 }
-      # This still doesnt work on docker?!
-      Process.send_after(peer, {:timeout}, 3000)
-    end)
+    if version == 1 do
+      IO.puts "Broadcast1 version 1"
+      Enum.map(peers, fn(peer) ->
+        send peer, { :broadcast, 1000, 3000}
+      end)
+    end
+
+    if version == 2 do
+      IO.puts "Broadcast1 version 2"
+      Enum.map(peers, fn(peer) ->
+        send peer, { :broadcast, 10_000_000, 3000}
+      end)
+    end
+
   end
+
 
 end
